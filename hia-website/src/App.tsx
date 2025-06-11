26d506f91c96b507dbd8e0780725b3494e7edb50
Monxey <isaac.plowman2005@gmail.com>
2025-06-11 13:00:01 -0400

    New custom HIA site
    replaced replit site code

---
+++ hia-website/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+      {/*Hero Section*/}
+      <div className='chromatic-glow'>
+        <p>Sadness everywhere.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
